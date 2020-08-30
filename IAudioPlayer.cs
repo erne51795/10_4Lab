384550145493ce73f3ef991ed1c96787fda2eb61
erne51795 <66523436+erne51795@users.noreply.github.com>
2020-08-30 06:39:03 -0500

    Add files via upload

---
+++ IAudioPlayer.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _10_4lab
+{
+    interface IAudioPlayer
+    {
+        void Play();
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _10_4lab
+{
+    class Program
+    {
+
+        public static void Main()
+        {
+
+            Artist muse = new _10_4lab.Artist("Muse");
+            muse.AddToPlaylist("Uprising");
+            muse.AddToPlaylist("Knights of Cydonia");
+            muse.AddToPlaylist("Madness");
+            muse.AddToPlaylist("Thought Contagion");
+
+            MediaPlayer player = new MediaPlayer();
+            player.PlayMedia(muse);
+
+            AudioBook howto = new AudioBook("How to Win Friends & Influence People", "Dale Carnegie");
+            player.PlayMedia(howto);
+
+            MovieSoundTrack starWars = new MovieSoundTrack("Star Wars");
+            starWars.SelectTrack("Opening Credits Theme");
+            player.PlayMedia(starWars);
+
+        }
+    }
+}
